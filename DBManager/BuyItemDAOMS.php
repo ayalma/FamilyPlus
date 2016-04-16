cf3bdb189be022364509e30cf54c65fb3f69c3dd
alimohammadi71 <alimohammadi350@gmail.com>
2016-04-16 12:48:15 +0430

    DbManager and Model package added

---
+++ DBManager/BuyItemDAOMS.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alimohammadi
+ * Date: 4/16/16
+ * Time: 12:43 PM
+ */
+class BuyItemDAOMS implements BuyItemDAO
+{
+
+}
---
+++ DBManager/EventsDAO.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alimohammadi
+ * Date: 4/16/16
+ * Time: 12:46 PM
+ */
+interface EventsDAO
+{
+
+}
