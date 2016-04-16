6f23a9f0fe972ac7313e33ea27571e57f1ecf884
Mohsen <mohsencomp90@gamil.com>
2016-04-16 13:58:43 +0430

    Add atribute and method to Group Class

--- Models/Group.php
+++ Models/Group.php
@@ -8,5 +8,69 @@
  */
 class Group
 {
+    private $_id; //Group ID
+    private $_admin; //Role of User
+    private $_users = array(); // lsit of each Groups Users
+
+    /**
+     * Group constructor.
+     * @param $_id
+     * @param $_admin
+     * @param array $_users
+     */
+    public function __construct($_id, $_admin, array $_users)
+    {
+        $this->_id = $_id;
+        $this->_admin = $_admin;
+        $this->_users = $_users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->_id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdmin()
+    {
+        return $this->_admin;
+    }
+
+    /**
+     * @param mixed $admin
+     */
+    public function setAdmin($admin)
+    {
+        $this->_admin = $admin;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUsers()
+    {
+        return $this->_users;
+    }
+
+    /**
+     * @param array $users
+     */
+    public function setUsers($users)
+    {
+        $this->_users = $users;
+    }
 
 }
