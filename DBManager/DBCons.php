f8d467545e1a1ea99f16477480ee450f0f2438e3
alimohammadi71 <alimohammadi350@gmail.com>
2016-04-16 20:39:37 +0430

    add DBCons class to DBManger package

---
+++ DBManager/DBCons.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alimohammadi
+ * Date: 4/16/16
+ * Time: 7:39 PM
+ */
+class DBCons
+{
+    public static $_host = "localhost:3306";
+    public static $_user = "root";
+    public static $_password = "gamor2012";
+    public static $db_name = "FamilyPlus";
+}
