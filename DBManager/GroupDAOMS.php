cdb1ac560ac98c38d9df320a83d988d31e616474
ayalma <alimohammadi7117@gmail.com>
2016-05-04 10:44:01 +0430

    implement save method  implement deletemember method implement savemember method

--- DBManager/GroupDAOMS.php
+++ DBManager/GroupDAOMS.php
@@ -55,4 +55,59 @@ class GroupDAOMS implements GroupDAO
 
         return $groups;
     }
+
+    /**
+     * @param Group $group : group to save.
+     * @return int         : id of group
+     */
+    public function save(Group $group)
+    {
+        $sql = 'INSERT INTO ' . DBCons::$_GROUP_TABLE
+            . ' (' . DBCons::$_GROUP_COL_ADMIN
+            . ', ' . DBCons::$_GROUP_COL_NAME . ') VALUES (?,?)';
+
+        $statement = $this->_connection->prepare($sql);
+
+        $statement->bind_param('ds', $group->getAdmin(), $group->getName());
+        $statement->execute();
+        $groupId = $statement->insert_id;
+
+        $statement->close();
+
+        return $groupId;
+
+    }
+
+    /**
+     * @param $groupId : id of group.
+     * @param $userId : id of group.
+     * @param $role : role of user in this group.
+     * @return boolean : status of adding as boolean.
+     */
+    public function saveMember($groupId, $userId, $role)
+    {
+        $sql = 'INSERT INTO ' . DBCons::$_GU_TABLE
+            . ' (' . DBCons::$_GU_COL_GROUP_ID
+            . ', ' . DBCons::$_GU_COL_USER_ID
+            . ', ' . DBCons::$_GU_COL_ROlE . ') VALUES (?,?,?)';
+
+        $statement = $this->_connection->prepare($sql);
+        $statement->bind_param('idi', $groupId, $userId, $role);
+
+        $res = $statement->execute();
+
+        $statement->close();
+
+        return $res;
+    }
+
+    /**
+     * @param $groupId : id of group.
+     * @param $userId : id of user.
+     * @return boolean : status of removing.
+     */
+    public function deleteMember($groupId, $userId)
+    {
+        // TODO: Implement deleteMember() method.
+    }
 }
--- Models/Group.php
+++ Models/Group.php
@@ -57,6 +57,24 @@ class Group
     {
         $this->_admin = $admin;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+    
+    
     
 
 }
